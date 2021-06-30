a95ab2755a8ff9b2ad1359c285f1446b4f1d6b0c
Thiago Vieira Pereira <th91vi@gmail.com>
2021-06-30 12:44:22 -0300

    feat: built code for SummaryForm

--- sundae-frontend/src/pages/summaryForm/SummaryForm.jsx
+++ sundae-frontend/src/pages/summaryForm/SummaryForm.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const SummaryForm = () => {
-  return <div></div>;
+  const [tcChecked, setChecked] = useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+    </span>
+  );
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
 };
 
 export default SummaryForm;
