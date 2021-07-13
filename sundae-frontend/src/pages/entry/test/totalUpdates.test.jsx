e3e69f57975c710f974af632fb66ee03f7eee32f
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-13 13:46:58 -0300

    feat: added text input test

---
+++ sundae-frontend/src/pages/entry/test/totalUpdates.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Options from "../Options";
+
+test("update scoop subtotal when scoops change", async () => {
+  render(<Options optionType="scoops" />);
+
+  // make sure total starts at 0.00
+  const scoopsSubtotal = screen.getByText("Scoops total: $", {
+    exact: false,
+  });
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  // update vanilla scoops
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
+
+  // update chocolate scoops
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, "2");
+  expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
