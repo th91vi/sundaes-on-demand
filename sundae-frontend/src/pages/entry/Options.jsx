cfa1d1173166bf0c3a6835081dc620c666be87ee
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-21 21:52:04 -0300

    feat: added logic to Options and ScoopOptions, using context data

--- sundae-frontend/src/pages/entry/Options.jsx
+++ sundae-frontend/src/pages/entry/Options.jsx
@@ -5,10 +5,13 @@ import baseURL from "../../utils/baseURL";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import AlertBanner from "../commom/AlertBanner";
+import { pricePerItem } from "../../constants/pricePerItem";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   const fetchScoops = async () => {
     try {
@@ -29,15 +32,28 @@ const Options = ({ optionType }) => {
 
   //@TODO: reṕlace with ToppingOption when available
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, optionType)
+      }
     />
   ));
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 };
 
 export default Options;
--- sundae-frontend/src/pages/entry/ScoopOption.jsx
+++ sundae-frontend/src/pages/entry/ScoopOption.jsx
@@ -1,7 +1,10 @@
-import { Col } from "react-bootstrap";
+import { Col, Form, Row } from "react-bootstrap";
 import baseURL from "../../utils/baseURL";
 
-const ScoopOption = ({ name, imagePath }) => {
+const ScoopOption = ({ name, imagePath, updateItemCount }) => {
+  const handleChange = (event) => {
+    updateItemCount(name, event.target.value);
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -9,6 +12,22 @@ const ScoopOption = ({ name, imagePath }) => {
         src={`${baseURL}/${imagePath}`}
         alt={`${name} scoop`}
       />
+      <Form.Group
+        controlId={`${name}-count`}
+        as={Row}
+        style={{ marginTop: "10px" }}
+      >
+        <Form.Label column xs="6" style={{ textAlign: "right" }}>
+          {name}
+        </Form.Label>
+        <Col xs="5" styçe={{ textAlign: "left" }}>
+          <Form.Control
+            type="number"
+            defaultValue={0}
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
     </Col>
   );
 };
