9c07a277ed2c7ce30d2b4a496a91683676028573
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-22 20:55:37 -0300

    feat: added custom render with context for tests

--- sundae-frontend/src/pages/entry/test/Options.test.jsx
+++ sundae-frontend/src/pages/entry/test/Options.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils/testing-libraries-utils";
 import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 import Options from "../Options";
---
+++ sundae-frontend/src/test-utils/testing-libraries-utils.js
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import { OrderDetailsProvider } from "../contexts/OrderDetails";
+
+const renderWithContext = (ui, options) =>
+  render(ui, {
+    wrapper: OrderDetailsProvider,
+    ...options,
+  });
+
+// re-export everuthing
+export * from "@testing-library/react";
+
+// override render method
+export { renderWithContext as render };
