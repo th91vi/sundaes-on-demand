9fa7d0ec0dfbd76054bae2d975f18e2e53e2563d
Thiago Vieira Pereira <th91vi@gmail.com>
2021-07-22 21:39:52 -0300

    feat: added toppings checkbox

--- sundae-frontend/src/pages/entry/ToppingOption.jsx
+++ sundae-frontend/src/pages/entry/ToppingOption.jsx
@@ -1,7 +1,7 @@
-import { Col } from "react-bootstrap";
+import { Col, Form } from "react-bootstrap";
 import baseURL from "../../utils/baseURL";
 
-const ToppingOption = ({ name, imagePath }) => {
+const ToppingOption = ({ name, imagePath, updateItemCount }) => {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -9,6 +9,13 @@ const ToppingOption = ({ name, imagePath }) => {
         src={`${baseURL}/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check
+          type="checkbox"
+          onChange={(e) => updateItemCount(name, e.target.checked ? 1 : 0)}
+          label={name}
+        />
+      </Form.Group>
     </Col>
   );
 };
